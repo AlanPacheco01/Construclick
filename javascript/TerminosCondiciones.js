10636e4b48217add1ce46671320dff1bb95e4b03
AlanPacheco01 <alan.pacheco.r01@gmail.com>
2024-08-15 21:50:50 -0600

    se añaden paginas de TyC y Avisos de Privacidad

--- javascript/TerminosCondiciones.js
+++ javascript/TerminosCondiciones.js
@@ -1,98 +1,45 @@
-const derechosArco = document.getElementById("DerechosArco");
-derechosArco.hidden =true;
-const datosPersonales = document.getElementById("DatosPersonales");
-datosPersonales.hidden = true;
-const identidadDom = document.getElementById("IdentidadDom");
-identidadDom.hidden = true;
-const tratamientoDatos = document.getElementById("TratamientoDatos");
-tratamientoDatos.hidden = true;
+const propInt = document.getElementById("propiedadIntelectual");
+propInt.hidden =true;
+const regCont = document.getElementById("registroContrasenas");
+regCont.hidden = true;
+const servApart = document.getElementById("servApart");
+servApart.hidden = true;
 
-const derechosArcoLi = document.getElementById("derechosArc");
-derechosArcoLi.addEventListener('click', ()=> {
-    derechosArco.hidden=false;
-    datosPersonales.hidden=true;
-    identidadDom.hidden = true;
-    tratamientoDatos.hidden = true;
+
+const propiedadIntLi = document.getElementById("propiedadInt");
+propiedadIntLi.addEventListener('click', ()=> {
+    propInt.hidden=false;
+    regCont.hidden=true;
+    servApart.hidden = true;
 })
-derechosArcoLi.addEventListener('mouseover',()=>{
-    derechosArco.hidden=false;
-    datosPersonales.hidden=true;
-    identidadDom.hidden = true;
-    tratamientoDatos.hidden = true;
+propiedadIntLi.addEventListener('mouseover',()=>{
+    propInt.hidden=false;
+    regCont.hidden=true;
+    servApart.hidden = true;
 })
 
-const datosPersonalesLi = document.getElementById("datosPer");
-datosPersonalesLi.addEventListener('click', ()=>{
-    derechosArco.hidden=true;
-    datosPersonales.hidden=false;
-    identidadDom.hidden = true;
-    tratamientoDatos.hidden = true;
+const registroContLi= document.getElementById("registroContra");
+registroContLi.addEventListener('click', ()=>{
+    propInt.hidden=true;
+    regCont.hidden=false;
+    servApart.hidden = true;
 })
 
-datosPersonalesLi.addEventListener('mouseover', ()=> {
-    derechosArco.hidden=true;
-    datosPersonales.hidden=false;
-    identidadDom.hidden = true;
-    tratamientoDatos.hidden = true; 
+registroContLi.addEventListener('mouseover', ()=> {
+    propInt.hidden=true;
+    regCont.hidden=false;
+    servApart.hidden = true;
 })
 
-const identidadDomLi = document.getElementById("identDom");
-identidadDomLi.addEventListener('click', ()=> {
-    derechosArco.hidden=true;
-    datosPersonales.hidden=true;
-    identidadDom.hidden=false;
-    tratamientoDatos.hidden = true;
+const serviciosApartLi = document.getElementById("serviciosApart");
+serviciosApartLi.addEventListener('click', ()=> {
+    propInt.hidden=true;
+    regCont.hidden=true;
+    servApart.hidden = false;
 });
 
-identidadDomLi.addEventListener('mouseover', ()=>{
-    derechosArco.hidden=true;
-    datosPersonales.hidden=true;
-    identidadDom.hidden=false;
-    tratamientoDatos.hidden = true;
+serviciosApartLi.addEventListener('mouseover', ()=>{
+    propInt.hidden=true;
+    regCont.hidden=true;
+    servApart.hidden = false;
 })
-
-const tratamientoDatosLi = document.getElementById("tratamDato");
-tratamientoDatosLi.addEventListener('click', ()=>{
-    derechosArco.hidden=true;
-    datosPersonales.hidden=true;
-    identidadDom.hidden=true;
-    tratamientoDatos.hidden = false;
-})
-
-tratamientoDatosLi.addEventListener('mouseover', ()=>{
-    derechosArco.hidden=true;
-    datosPersonales.hidden=true;
-    identidadDom.hidden=true;
-    tratamientoDatos.hidden = false;
-})
-/*
-const ocultarTodo = document.getElementById('main-container');
-ocultarTodo.addEventListener('click', ()=>{
-    derechosArco.hidden=true;
-    datosPersonales.hidden=true;
-    identidadDom.hidden=true;
-    tratamientoDatos.hidden = true;
-})
-    */
-/*
-let x = window.matchMedia('(max-width: 700px)');
-
-function cambioPantalla(x){
-    if(x.matches){
-        derechosArcoLi.addEventListener('click', ()=> {
-            derechosArco.hidden = false;
-            datosPersonales.hidden = true;
-            identidadDom.hidden = true;
-            tratamientoDatos.hidden = true;
-        });
-    }
-}
-
-// Run the function at least once on page load
-cambioPantalla(x);
-
-// Listen for changes in screen size and run the function when a change occurs
-x.addEventListener('change', () => {
-    cambioPantalla(x);
-});
-*/
