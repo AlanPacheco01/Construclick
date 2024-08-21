9945c05a8b1e93339c0d06332880fcf77296cf90
Sebfria <sebichusedge@gmail.com>
2024-08-21 12:50:28 -0600

    Actualización
    añadí el evento para que al momento de seleccionar alguna opcion la pagina redirija, ademas en caso de no seleccionar ninguna, aparecera por pantalla una alerta para el usuario

--- javascript/registropage1.js
+++ javascript/registropage1.js
@@ -1,8 +1,28 @@
-const seletElement=document.getElementById('lenguajes');
-console.log(lenguajes);
-const linkElement=document.getElementById('gofuture')
-seletElement.addEventListener('change', ()=>{
-   const selectedValue=seletElement.value;
-   linkElement.href=selectedValue;
+document.getElementById("register--alert").hidden = true;
 
+const seletElement = document.getElementById('whatsPromoted');
+const linkElement = document.getElementById('pages');
+const continueButton = document.getElementById('form--continue');
+
+continueButton.addEventListener('click', (event) => {
+    // Prevenir el envío del formulario para manejar la lógica primero
+    event.preventDefault();
+    
+    // Obtener el valor seleccionado del <select>
+    const selectedValue = seletElement.value;
+
+    // Si el valor seleccionado es "Categorias", mostrar el alert
+    if (selectedValue != "Categorias") {
+         // Ocultar el alert y cambiar el href del linkElement
+         document.getElementById("register--alert").hidden = true;
+         //NOT
+         linkElement.href=selectedValue;
+         // Aqui deberias redigirir manualmente si quieres cambiar de pagina
+         window.location.href = selectedValue;
+        
+    } else {
+        document.getElementById("register--alert").hidden = false;
+        // Opcionalmente, podrías enviar el formulario aquí si es necesario
+        // document.querySelector('form').submit();
+    }
 });
