6905e3308761e19d22248d56d7f1a5dc774c64d0
Alan Alberto Pacheco Ramírez <alan.pacheco.r.01@gmail.com>
2024-08-22 10:25:52 -0600

    Actualización aviso de privacidad js
    se documenta el código para facilitar su lectura

--- javascript/AvisoPrivacidad.js
+++ javascript/AvisoPrivacidad.js
@@ -1,3 +1,4 @@
+//Definición del estado de los textos, hidden por defecto
 const derechosArco = document.getElementById("DerechosArco");
 derechosArco.hidden =true;
 const datosPersonales = document.getElementById("DatosPersonales");
@@ -7,13 +8,18 @@ identidadDom.hidden = true;
 const tratamientoDatos = document.getElementById("TratamientoDatos");
 tratamientoDatos.hidden = true;
 
+//Evento para mostrar la sección de derechos
 const derechosArcoLi = document.getElementById("derechosArc");
+
+//Evento para la versión movil
 derechosArcoLi.addEventListener('click', ()=> {
     derechosArco.hidden=false;
     datosPersonales.hidden=true;
     identidadDom.hidden = true;
     tratamientoDatos.hidden = true;
 })
+
+//Evento para la versión escritorio
 derechosArcoLi.addEventListener('mouseover',()=>{
     derechosArco.hidden=false;
     datosPersonales.hidden=true;
@@ -21,7 +27,10 @@ derechosArcoLi.addEventListener('mouseover',()=>{
     tratamientoDatos.hidden = true;
 })
 
+//Evento para mostrar la sección de datos personales
 const datosPersonalesLi = document.getElementById("datosPer");
+
+//Evento para la versión movil
 datosPersonalesLi.addEventListener('click', ()=>{
     derechosArco.hidden=true;
     datosPersonales.hidden=false;
@@ -29,6 +38,7 @@ datosPersonalesLi.addEventListener('click', ()=>{
     tratamientoDatos.hidden = true;
 })
 
+//Evento para la versión de escritorio
 datosPersonalesLi.addEventListener('mouseover', ()=> {
     derechosArco.hidden=true;
     datosPersonales.hidden=false;
@@ -36,7 +46,10 @@ datosPersonalesLi.addEventListener('mouseover', ()=> {
     tratamientoDatos.hidden = true; 
 })
 
+//Evento para mostrar la sección de identidad
 const identidadDomLi = document.getElementById("identDom");
+
+//Evento para la versión móvil
 identidadDomLi.addEventListener('click', ()=> {
     derechosArco.hidden=true;
     datosPersonales.hidden=true;
@@ -44,6 +57,7 @@ identidadDomLi.addEventListener('click', ()=> {
     tratamientoDatos.hidden = true;
 });
 
+//Evento para la versión de escritorio
 identidadDomLi.addEventListener('mouseover', ()=>{
     derechosArco.hidden=true;
     datosPersonales.hidden=true;
@@ -51,7 +65,10 @@ identidadDomLi.addEventListener('mouseover', ()=>{
     tratamientoDatos.hidden = true;
 })
 
+//Evento para mostrar la sección de datos personales
 const tratamientoDatosLi = document.getElementById("tratamDato");
+
+//Evento para la versión movil
 tratamientoDatosLi.addEventListener('click', ()=>{
     derechosArco.hidden=true;
     datosPersonales.hidden=true;
@@ -59,40 +76,10 @@ tratamientoDatosLi.addEventListener('click', ()=>{
     tratamientoDatos.hidden = false;
 })
 
+//Evento para la versión de escritorio
 tratamientoDatosLi.addEventListener('mouseover', ()=>{
     derechosArco.hidden=true;
     datosPersonales.hidden=true;
     identidadDom.hidden=true;
     tratamientoDatos.hidden = false;
 })
-/*
-const ocultarTodo = document.getElementById('main-container');
-ocultarTodo.addEventListener('click', ()=>{
-    derechosArco.hidden=true;
-    datosPersonales.hidden=true;
-    identidadDom.hidden=true;
-    tratamientoDatos.hidden = true;
-})
-    */
-/*
-let x = window.matchMedia('(max-width: 700px)');
-
-function cambioPantalla(x){
-    if(x.matches){
-        derechosArcoLi.addEventListener('click', ()=> {
-            derechosArco.hidden = false;
-            datosPersonales.hidden = true;
-            identidadDom.hidden = true;
-            tratamientoDatos.hidden = true;
-        });
-    }
-}
-
-// Run the function at least once on page load
-cambioPantalla(x);
-
-// Listen for changes in screen size and run the function when a change occurs
-x.addEventListener('change', () => {
-    cambioPantalla(x);
-});
-*/
