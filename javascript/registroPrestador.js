0067cd14d9ea993c2d6cd2c27ff5610375a902dd
Alan Alberto Pacheco Ramírez <alan.pacheco.r.01@gmail.com>
2024-09-06 11:42:10 -0600

    Update registroPrestador.js
    actualización de las alertas y conexión con el back

--- javascript/registroPrestador.js
+++ javascript/registroPrestador.js
@@ -1,111 +1,145 @@
+//atributos de las alertas, hidden por defecto
+const nameAlert = document.getElementById("name--alert");
+nameAlert.hidden = true;
+const correoAlert = document.getElementById("email--alert");
+correoAlert.hidden = true;
+const contrasenaAlert = document.getElementById("password--alert");
+contrasenaAlert.hidden = true;
+const contrasenaCoincide = document.getElementById("password2--alert");
+contrasenaCoincide.hidden = true;
+const telefonoAlert = document.getElementById("phone--alert");
+telefonoAlert.hidden = true;
+const serviciosAlert = document.getElementById("place--alert");
+serviciosAlert.hidden = true;
+const ineAlert = document.getElementById("ine--alert");
+ineAlert.hidden = true;
+const savedAlert = document.getElementById("saved--alert");
+savedAlert.hidden = true;
+
+//boton del formulario, desactivado por default
+const botonEnvio = document.getElementById("send--form");
+botonEnvio.disabled = true;
+//información de los botones
+const placeOptions = document.getElementById("placeOption");
+
+//alertas para el evento de la promoción de servicios dentro de cdmx o edomex
+placeOptions.addEventListener('change', () => {
+    if (placeOptions.value === "No") {
+        serviciosAlert.hidden = false;
+    } else {
+        serviciosAlert.hidden = true;
+    }
+})
+
 
 //Recepción de los datos del usuario en el formulario
 const formServices = document.getElementById('RegistroPrestador');
-//obteniendo informaciónd del boton
-const button = document.getElementById('send--form');
-const buttonPlace = document.getElementById('buttons');
 
-const buttonYes = document.getElementById('cdmx--yes');
-const buttonNo = document.getElementById('cdmx--no');
-
-    placeOption.addEventListener('click', function seleccionarOpcion() {
-    //Leemos el valor del select
-    let placeOption = document.getElementById("place");
-    document.querySelector("#place").value = place;
-    if(value == "si"){
-    document.getElementById("place--alert").hidden=true;
-    }
-    if(value == "no"){
-    document.getElementById("place--alert").hidden=false;
-    }
-    if (valor = null) {
-    document.getElementById("place--alert2").hidden=false;
-    }
-    })
-
-
-    function seleccionarOpcion(){
-        document.querySelector("#place").value = place;
-    }
-
-
-//detonación del evento
+//Evento del formulario
 formServices.addEventListener('submit', (event) => {
     event.preventDefault();
-    //Lectura del nombre del usuario
-    const nombre = document.getElementById('userName').value
-    //validación del nombre del usuario mediante regex
-    const nombreRegex = /^[a-zA-ZáéíóúüñÁÉÍÓÚÜÑ\s]+$/;   //nota :el \s permite que haya espacios
-    //Lectura del correo del usuario
-    const correo = document.getElementById('userEmail').value
-    //validación del correo del usuario mediante regex
+
+    //lectura de los datos del usuario e implementación de las regex
+    //Nombre de usuario
+    const nombre = document.getElementById("userName").value;
+    const nombreRegex = /^[a-zA-ZáéíóúüñÁÉÍÓÚÜÑ\s]+$/;
+
+    //Correo del usuario
+    const email = document.getElementById("userEmail").value;
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    //lectura del teléfono del usuario
-    const telefono = document.getElementById('userPhone').value
-    //validación del teléfono del usuario mediante regex
+
+    //Teléfono del usuario
+    const telefono = document.getElementById("userPhone").value;
     const telefonoRegex = /^\d{10}$/;
-    //lectura de la contraseña del usuario
-    const contraseña = document.getElementById('contraseña').value
-    //Validación de de la contraseña mediante regex
-    const contraseñaRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
-    //Lectura de confirmacion de contraseña del usuario
-    const confirmaContraseña = document.getElementById('confirmaContraseña').value
+
+    //Constraseña del usuario
+    const contrasena = document.getElementById("contrasena").value;
+    const constrasenaRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
+
+    //Consfirmación contraseña del usuario
+    const contrasenaMatch = document.getElementById("confirmaContrasena").value;
+
     //carga de INE
-    const ineCarga = document.getElementById('ine--alert').value;
-    
-    //Condicionales que le dicen al usuario que los datos son erroneos
-        if (!nombreRegex.test(nombre)) {
-            document.getElementById("name--alert").hidden=false;
-        } else{
-            document.getElementById("name--alert").hidden=true;
-        }
-        if (!emailRegex.test(correo)) {
-            document.getElementById("email--alert").hidden=false;
-        } else{
-            document.getElementById("email--alert").hidden=true;
-        }
+    const ineCarga = document.getElementById("ineCarga").files.length>0;
 
-        if(!contraseñaRegex.test(contraseña)){
-            document.getElementById("password--alert").hidden=false;
-        } else{
-            document.getElementById("password--alert").hidden=true;
-        }
-        //Validacion de contraseña
-        if (contraseña === confirmaContraseña) {
-            document.getElementById("password2--alert").hidden=true;
-        } else{
-            document.getElementById("password2--alert").hidden=false;
-        }
+    //Validación de los campos del formulario
+    let validacion=true;
 
-        if (!telefonoRegex.test(telefono)) {
-            document.getElementById("phone--alert").hidden=false;
-        } else{
-            document.getElementById("phone--alert").hidden=true;
-        }
-
-        if (ineCarga) {
-            document.getElementById("ine--alert").hidden=true;
-        } else {
-            document.getElementById("ine--alert").hidden=false;
-        }
-        if (referencia) {
-            document.getElementById("referencia--alert").hidden=true;
-        } else {
-            document.getElementById("referencia--alert").hidden=false;
-        }
+    if (!nombreRegex.test(nombre)) {
+        nameAlert.hidden = false;
+        validacion = false;
+    } else {
+        nameAlert.hidden = true;
         
+    }
+    if (!emailRegex.test(email)) {
+        correoAlert.hidden = false;
+        validacion = false;
+    } else {
+        correoAlert.hidden = true;
+        
+    }
+    if (!constrasenaRegex.test(contrasena)) {
+        contrasenaAlert.hidden = false;
+        validacion = false;
+    } else {
+        contrasenaAlert.hidden = true;
+        
+    }
+    if (contrasena !== contrasenaMatch) {
+        contrasenaCoincide.hidden = false;
+        validacion = false;
+    } else {
+        contrasenaCoincide.hidden = true;
+        
+    }
+    if (!ineCarga) {
+        ineAlert.hidden = false;
+        validacion = false;
+    } else {
+        ineAlert.hidden = true;
+        
+    }
+    if (!telefonoRegex.test(telefono)) {
+        telefonoAlert.hidden = false;
+        validacion = false;
+    } else {
+        telefonoAlert.hidden = true;
+        
+    }
+
     //Almacenamiento local de los datos ingresados en el form
-        localStorage.setItem('name', nombre)
-        localStorage.setItem('fotoPerfil', foto)
-        localStorage.setItem('email', correo)
-        localStorage.setItem('contraseña', contraseña)
-        localStorage.setItem('confirmaContraseña', confirmaContraseña)
-        localStorage.setItem('telephone', telefono)
-        localStorage.setItem('LugarServicios', botonSi)
-        localStorage.setItem('INE', ineCarga)
-        localStorage.setItem('referenciaTrabajo', referencia)
-        if (nombreRegex.test(nombre) && emailRegex.test(correo) && telefonoRegex.test(telefono) && contraseñaRegex.test(contraseña) && contraseña === confirmaContraseña && botonSi && ineCarga &&  referencia) {
-            document.getElementById("saved--alert").hidden=false;
-        }
-    
-}) 
+    localStorage.setItem('name', nombre)
+    localStorage.setItem('email', email)
+    localStorage.setItem('contraseña', contrasena)
+    localStorage.setItem('confirmaContraseña', contrasenaMatch)
+    localStorage.setItem('telephone', telefono)
+    localStorage.setItem('INE', ineCarga)
+
+   
+    savedAlert.hidden = false;
+
+// Conexión con el backend
+const user = {
+    'username': nombre,
+    'password': contrasena,
+    'email': email,
+    'telephone': telefono
+};
+
+const URL = `http://3.144.151.137localhost:8080/registro/r1`;
+
+fetch(URL, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+})
+.then(response => response.json())
+.then(data => console.log('Guardado', data))
+.catch(error => console.error('Error:', error));
+
+})
+formServices.reset()
+
